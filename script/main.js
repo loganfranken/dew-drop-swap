57ee3c2a8533f73af82ed94f8805e649647656af
Logan Franken <loganfranken@gmail.com>
2019-04-12 23:28:04 -0700

    Created basic PhaserJS project

--- script/main.js
+++ script/main.js
@@ -1,1 +1,28 @@
-console.log('Test');
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+const game = new Phaser.Game(config);
+
+function preload()
+{
+    this.load.image('tile_01', 'assets/tile_01.png');
+    this.load.image('tile_02', 'assets/tile_02.png');
+    this.load.image('tile_03', 'assets/tile_03.png');
+    this.load.image('tile_04', 'assets/tile_04.png');
+}
+
+function create()
+{
+    this.add.image(400, 300, 'tile_01');
+}
+
+function update() {
+}
