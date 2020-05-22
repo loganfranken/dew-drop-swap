00765be84329c411672f72a002e386c82adbe491
Logan Franken <loganfranken@gmail.com>
2020-05-21 23:02:03 -0700

    Added basic title graphics

--- script/scenes/TitleScene.js
+++ script/scenes/TitleScene.js
@@ -5,16 +5,30 @@ export default class extends Phaser.Scene {
         super('TitleScene');
     }
 
+    preload()
+    {
+        this.load.image('background', 'assets/background.png');
+        this.load.image('title', 'assets/title.png');
+        this.load.image('start', 'assets/start.png');
+    }
+
     create()
     {
         const self = this;
 
+        // Background
+        this.add.image(400, 400, 'background');
+
         // Title
-        this.add.text(50, 50, 'Match Three!');
+        this.add.image(400, 200, 'title');
 
         // "Start" Button
-        const startBtn = this.add.text(50, 70, 'Start Game');
+        const startBtn = this.add.image(400, 600, 'start');
         startBtn.setInteractive();
+
+        startBtn.on('pointerover', () => { startBtn.setTintFill(0xffffff); });
+        startBtn.on('pointerout', () => { startBtn.clearTint(); });
+
         startBtn.on('pointerdown', () => { self.scene.start('RoundScene', { level: 0 }); });
     }
     
