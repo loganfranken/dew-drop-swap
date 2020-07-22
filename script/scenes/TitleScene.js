34982ca63d6d5c626f07063efef82addddf30879
Logan Franken <loganfranken@gmail.com>
2020-07-21 23:58:12 -0700

    Added hover effect to "Start" on title screen

--- script/scenes/TitleScene.js
+++ script/scenes/TitleScene.js
@@ -10,6 +10,8 @@ export default class extends Phaser.Scene {
         this.load.image('background', 'assets/background.png');
         this.load.image('title', 'assets/title.png');
         this.load.image('start', 'assets/start.png');
+        this.load.image('start-glow', 'assets/start_glow.png');
+        this.load.image('start-active', 'assets/start_active.png');
     }
 
     create()
@@ -23,11 +25,41 @@ export default class extends Phaser.Scene {
         this.add.image(400, 200, 'title');
 
         // "Start" Button
-        const startBtn = this.add.image(400, 600, 'start');
-        startBtn.setInteractive();
+        const startBtnGlow = this.add.image(400, 600, 'start-glow');
+        startBtnGlow.setAlpha(0);
 
-        startBtn.on('pointerover', () => { startBtn.setTintFill(0xffffff); });
-        startBtn.on('pointerout', () => { startBtn.clearTint(); });
+        this.tweens.add({
+            targets: startBtnGlow,
+            alpha: 1,
+            duration: 1000,
+            repeat: -1,
+            yoyo: true,
+            ease: 'Sine.easeInOut'
+        });
+
+        const startBtn = this.add.image(400, 600, 'start');
+        startBtn.setInteractive({ cursor: 'pointer' });
+
+        const startBtnOverlay = this.add.image(400, 600, 'start-active');
+        startBtnOverlay.setAlpha(0);
+
+        startBtn.on('pointerover', () => {
+            self.tweens.add({
+                targets: startBtnOverlay,
+                alpha: 1,
+                duration: 100,
+                ease: 'Sine.easeInOut'
+            });
+        });
+
+        startBtn.on('pointerout', () => {
+            self.tweens.add({
+                targets: startBtnOverlay,
+                alpha: 0,
+                duration: 100,
+                ease: 'Sine.easeInOut'
+            });
+        });
 
         startBtn.on('pointerdown', () => { self.scene.start('RoundScene', { level: 0 }); });
     }
