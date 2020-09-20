2c2e07ed856df8d62b5e836c69197d5014fb0806
Logan Franken <loganfranken@gmail.com>
2020-09-20 01:45:14 -0700

    Timer now displays level ticks right away

--- script/Timer.js
+++ script/Timer.js
@@ -2,12 +2,12 @@ import FontStyleManifest from './FontStyleManifest';
 
 export default class {
 
-    constructor(x, y, seconds)
+    constructor(x, y, ticks)
     {
         this.x = x;
         this.y = y;
         this.text = null;
-        this.seconds = seconds;
+        this.ticks = ticks;
         this.isPaused = true;
     }
 
@@ -17,7 +17,7 @@ export default class {
         context.add.image(this.x + 15, this.y + 15, 'icon_timer').setAlpha(0.7);
 
         // Text
-        this.text = context.add.text(this.x + 40, this.y, this.score, FontStyleManifest.Default);
+        this.text = context.add.text(this.x + 40, this.y, this.getTimeOutput(this.ticks), FontStyleManifest.Default);
         
         this.tick();
         context.time.addEvent({ delay: 1000, callback: this.tick, callbackScope: this, loop: true })
@@ -25,27 +25,31 @@ export default class {
 
     tick()
     {
-        if(this.seconds <= 0 || this.isPaused)
+        if(this.ticks <= 0 || this.isPaused)
         {
             return;
         }
 
-        this.seconds--;
+        this.ticks--;
 
-        // Display the remaining time
-        const minutes = Math.floor(this.seconds / 60);
-
-        let seconds = (this.seconds % 60);
-        if(seconds < 10)
-        {
-            seconds = '0' + seconds;
-        }
-
-        this.text.setText(`${minutes}:${seconds}`);
+        this.text.setText(this.getTimeOutput(this.ticks));
     }
 
     start()
     {
         this.isPaused = false;
+    }
+
+    getTimeOutput(input)
+    {
+        const minutes = Math.floor(input / 60);
+
+        let seconds = (input % 60);
+        if(seconds < 10)
+        {
+            seconds = '0' + seconds;
+        }
+
+        return `${minutes}:${seconds}`;
     }
 }
